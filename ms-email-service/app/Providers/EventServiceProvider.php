86da4aa607b7d3fbd1f34eff38571e6cb0bb896b
abdalla maher <abdallamaher89@gmail.com>
2022-03-27 02:21:18 +0200

    add RabbitMQ to ms-email-service

--- ms-email-service/app/Providers/EventServiceProvider.php
+++ ms-email-service/app/Providers/EventServiceProvider.php
@@ -27,6 +27,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \App::bindMethod(AdminAdded::class . '@handle', fn($job) => $job->handle());
     }
 }
